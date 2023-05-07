4c7ac191742166212a080d5c958fc432a809e76e
Dipankar Majee <dipankar.majee@simplyfi.tech>
2023-05-07 16:04:02 +0530

    complete practice

--- src/component/CarList.js
+++ src/component/CarList.js
@@ -3,7 +3,14 @@ import { deleteCar } from "../store";
 
 const CarList = () => {
   const dispatch = useDispatch();
-  const carData = useSelector((state) => state.cars.carData);
+  const { carData, name } = useSelector(
+    ({ form, cars: { carData, searchTerm } }) => {
+      const filterCar = carData.filter((eachCar) =>
+        eachCar.name.includes(searchTerm)
+      );
+      return { carData: filterCar, name: form.name };
+    }
+  );
 
   const onClickDelete = (id) => {
     console.log(id);
@@ -16,9 +23,14 @@ const CarList = () => {
           key={eachCarData.id}
           className="d-flex justify-content-between align-items-center w-100 border p-1 rounded"
         >
-          <span>
-            {eachCarData.name} Rs.{eachCarData.cost}
-          </span>
+          <div className="d-flex gap-2">
+            <span
+              className={name && eachCarData.name.includes(name) && "fw-bold"}
+            >
+              {eachCarData.name}
+            </span>
+            <span>Rs.{eachCarData.cost}</span>
+          </div>
           <button
             onClick={() => onClickDelete(eachCarData.id)}
             className="btn btn-danger px-5 py-2"
