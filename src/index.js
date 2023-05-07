e455b7a0c58987d7ddb5d745b50f33e2c49c8059
Dipankar Majee <dipankar.majee@simplyfi.tech>
2023-05-07 22:03:58 +0530

    create store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import App from "./App";
 
 // Bootstrap CSS
 import "bootstrap/dist/css/bootstrap.min.css";
 // Bootstrap Bundle JS
 import "bootstrap/dist/js/bootstrap.bundle.min";
+import { store } from "./store";
 
 const element = document.getElementById("root");
 const root = createRoot(element);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
